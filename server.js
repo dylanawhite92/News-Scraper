8f24702945eb4f75a4379d9385f030fdc5b3e704
Dylan White <dylanawhite11@gmail.com>
2019-01-16 22:48:57 -0600

    updated routes

--- server.js
+++ server.js
@@ -83,80 +83,96 @@ app.get("/articles", function(req, res) {
       });
   });
   
-  // Route for creating an Article in the db
-  app.post("/api/saved", function(req, res) {
-    db.Article.create(req.body)
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+    db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function(dbArticle) {
+        res.json(dbArticle);
+    }).catch(function(err) {
+        res.json(err);
+    });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+    db.Note.create(req.body)
+    .then(function(dbNote) {
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    }).then(function(dbArticle) {
+        res.json(dbArticle);
+    }).catch(function(err) {
+        res.json(err);
+    });
+});
+  
+//Route for deleting an article from the db
+app.delete("/delete/:id", function(req, res) {
+    db.Article.deleteOne({ _id: req.params.id })
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    }).catch(function(err) {
+        // If an error occurred, send it to the client
+          res.json(err);
+      });
+  });
+  
+//Route for deleting a note
+app.delete("/articles/:id", function(req, res) {
+    db.Note.deleteOne({ _id: req.params.id })
+    .then(function(removed) {
+        res.json(removed);
+    }).catch(function(err,removed) {
+        // If an error occurred, send it to the client
+        res.json(err);
+    });
+});
+  
+// Route for saving/updating an Article's Note
+app.post("/articles/:id", function(req, res) {
+    db.Note.create(req.body).then(function(dbNote) {
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    }).then(function(dbArticle) {
+        res.json(dbArticle);
+    })
+    .catch(function(err) {
+        res.json(err);
+    });
+});
+
+// Route for saving/updating article to be saved
+app.put("/saved/:id", function(req, res) {
+    db.Article.findByIdAndUpdate({ _id: req.params.id }, { $set: { isSaved: true }})
+    .then(function(dbArticle) {
+        res.json(dbArticle);
+    })
+    .catch(function(err) {
+        res.json(err);
+    });
+});
+  
+// Route for getting saved article
+app.get("/saved", function(req, res) {
+    db.Article.find({ isSaved: true })
+    .then(function(dbArticle) {
+        res.json(dbArticle);
+    })
+    .catch(function(err) {
+        res.json(err);
+    });
+});
+  
+  // Route for deleting/updating saved article
+  app.put("/delete/:id", function(req, res) {
+  
+    db.Article
+      .findByIdAndUpdate({ _id: req.params.id }, { $set: { isSaved: false }})
       .then(function(dbArticle) {
         res.json(dbArticle);
       })
       .catch(function(err) {
-        // If an error occurred, send it to the client
         res.json(err);
       });
-  });
-  
-  // Route for grabbing a specific Article by id, populate it with it's note
-  app.get("/articles/:id", function(req, res) {
-    console.log(req.params.id);
-    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-    db.Article.findOne({ _id: req.params.id })
-      // ..and populate all of the notes associated with it
-      .populate("note")
-      .then(function(dbArticle) {
-        // If we were able to successfully find an Article with the given id, send it back to the client
-        console.log(dbArticle);
-        if (dbArticle) {
-        res.render("articles", {
-          data: dbArticle
-        });
-      }
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
-  });
-  
-  //Route for deleting an article from the db
-  app.delete("/saved/:id", function(req, res) {
-    db.Article.deleteOne({ _id: req.params.id })
-    .then(function(removed) {
-      res.json(removed);
-    }).catch(function(err,removed) {
-        // If an error occurred, send it to the client
-          res.json(err);
-      });
-  });
-  
-  //Route for deleting a note
-  app.delete("/articles/:id", function(req, res) {
-    db.Note.deleteOne({ _id: req.params.id })
-    .then(function(removed) {
-      res.json(removed);
-    }).catch(function(err,removed) {
-        // If an error occurred, send it to the client
-          res.json(err);
-      });
-  });
-  
-  // Route for saving/updating an Article's associated Note
-  app.post("/articles/:id", function(req, res) {
-    // Create a new note and pass the req.body to the entry
-    db.Note.create(req.body)
-      .then(function(dbNote) {
-        db.Article.findOneAndUpdate({ _id: req.params.id }, {$push: { note: dbNote._id }}, { new: true })
-        .then(function(dbArticle) {
-          console.log(dbArticle);
-          res.json(dbArticle);
-        })
-        .catch(function(err) {
-          // If an error occurred, send it to the client
-          res.json(err);
-        });
-      })
-      .catch(function(err) {
-        res.json(err);
-      })
   });
 
 // Start the server
